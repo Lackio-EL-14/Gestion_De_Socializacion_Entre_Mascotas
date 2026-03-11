532141aabc27ed1e04817f96bab6f7195afedc15
atalasluis <atalasturey@gmail.com>
2026-03-11 00:09:23 -0400

    feat:card de mascota terminada

--- backend/src/modules/pets/pets.module.ts
+++ backend/src/modules/pets/pets.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { Pet } from './entities/pet.entity';
+import { PetsService } from './service/pets.service';
+import { PetsController } from './controller/pets.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Pet])],
+  controllers: [PetsController],
+  providers: [PetsService],
+})
 export class PetsModule {}
