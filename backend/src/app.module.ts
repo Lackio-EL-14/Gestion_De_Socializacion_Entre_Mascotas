4a47aafc97546ba900972bf3e3f950719b36ea88
Metzar <metzariel-14@localhost.localdomain>
2026-03-31 18:38:49 -0400

    Revert "feat: sincronizacion de la DB interrumpida para la presentacion"
    This reverts commit e14aeb4f45503d04824310517aee48675f158c4b.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -33,7 +33,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
               password: process.env.DB_PASSWORD,
               database: process.env.DB_DATABASE,
               autoLoadEntities: true,
-              synchronize: false,  //Cuidadito huaynaboy
+              synchronize: true,
 
               ssl: {
                 rejectUnauthorized: false,
