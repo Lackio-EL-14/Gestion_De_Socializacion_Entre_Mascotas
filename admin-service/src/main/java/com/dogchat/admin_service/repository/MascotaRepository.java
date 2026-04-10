eea1f63c79b526c24459a018d7786fc47c2a9a65
Metzar <metzariel-14@localhost.localdomain>
2026-04-10 00:08:37 -0400

    feat:limpieza absoluta de secretos

---
+++ admin-service/src/main/java/com/dogchat/admin_service/repository/MascotaRepository.java
@@ -0,0 +1,9 @@
+package com.dogchat.admin_service.repository;
+
+import com.dogchat.admin_service.entity.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Integer> {
+}
