ebb3eb279b90bcbb0347827b5c3b723a70547651
AlejoHuay <alvarez.h.alejo@gmail.com>
2026-04-14 11:14:28 -0400

    fix: Agrego multiidioma en pantalla create-report

--- frontend/src/app/features/reports/reports-module.ts
+++ frontend/src/app/features/reports/reports-module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { ReportsRoutingModule } from './reports-routing-module';
 import { CreateReportComponent } from './create-report/create-report';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [CreateReportComponent],
-  imports: [CommonModule, ReportsRoutingModule, FormsModule],
+  imports: [CommonModule, ReportsRoutingModule, FormsModule, TranslateModule],
 })
 export class ReportsModule {}
